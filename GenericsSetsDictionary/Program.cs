350dae27f4349d4517d41a409ee6154e9a71e3f3
Breno Ramon <breeramon@gmail.com>
2023-05-29 10:22:29 -0300

    Exercicio 1 sobre os Generics

---
+++ GenericsSetsDictionary/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsSetsDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintService<string> printService = new PrintService<string>();
+
+            Console.Write("How many values? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string x = Console.ReadLine();
+                printService.AddValue(x);
+            }
+
+            printService.Print();
+            Console.WriteLine("First: " + printService.First());
+
+            //A IMPLEMENTAÇÃO DE CIMA É COM STRINGS E A DE BAIXO COM INTEIROS
+
+            /*PrintService<int> printService = new PrintService<int>();
+
+            Console.Write("How many values? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                printService.AddValue(x);
+            }
+
+            printService.Print();
+            Console.WriteLine("First: " + printService.First());
+            */
+        }
+    }
+}
